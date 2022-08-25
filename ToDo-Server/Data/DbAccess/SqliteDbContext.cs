1b9caed51af990eaf423d55e2e1f3d94a6e5e6be
Canaan <reakak1@gmail.com>
2022-08-25 07:48:13 +0300

    Add: DbContext class

---
+++ ToDo-Server/Data/DbAccess/SqliteDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using ToDo_Server.Data.Models;
+
+namespace ToDo_Server.Data.DbAccess
+{
+    public class SqliteDbContext : DbContext
+    {
+        public SqliteDbContext(DbContextOptions<SqliteDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ToDo> ToDos => Set<ToDo>();
+    }
+}
