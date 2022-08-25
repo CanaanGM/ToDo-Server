4cc5c0c659819970e4124b9685a7d35ed32d49f8
Canaan <reakak1@gmail.com>
2022-08-25 07:48:36 +0300

    Add: Domain Model

---
+++ ToDo-Server/Data/Models/ToDo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDo_Server.Data.Models
+{
+    public class ToDo
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime Created { get; set; }
+        public DateTime MarkedDone { get; set; }
+        public DateTime AddedToDatabase { get; } = DateTime.UtcNow;
+        public bool status { get; set; } = false;
+
+        // user goes here ~!
+
+    }
+}
