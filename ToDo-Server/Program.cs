9693379e5605625a32a7fc8f6e1ce9d8c6dd7c67
Canaan <reakak1@gmail.com>
2022-08-25 09:43:38 +0300

    Add: more services configuration;(CORS, security, ... etc) Change: swagger auth via token bearer

--- ToDo-Server/Program.cs
+++ ToDo-Server/Program.cs
@@ -1,23 +1,99 @@
-using Microsoft.EntityFrameworkCore;
+using MediatR;
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+using System.Text;
+
+using ToDo_Server.Application.Interfaces;
+using ToDo_Server.Application.ToDo;
 using ToDo_Server.Data.DbAccess;
 using ToDo_Server.Data.Mapper;
+using ToDo_Server.Data.Models;
+using ToDo_Server.Infrastructure.Security;
+using ToDo_Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "ToDO server",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact { Email = "canaand1@gmail.com", Name = "Canaan" },
+        Description = "ToDO Server",
+        Version = "v1"
+    });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 
 //! extract into an extension class
 builder.Services.AddAutoMapper(typeof(MapperProfiles).Assembly);
-builder.Services.AddDbContext<SqliteDbContext>(opt => opt.UseSqlite(
+builder.Services.AddMediatR(typeof(Create.Handler).Assembly);
+builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlite(
     builder.Configuration["ConnectionStrings:SqliteTempBase"]
     ));
+
+builder.Services.AddScoped<IUserAccessor, UserAccessor>();
+
+builder.Services.AddCors(options => options.AddPolicy("CorsPolicy", policy =>
+{
+    policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+}));
+
+
+builder.Services.AddIdentityCore<AppUser>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+})
+    .AddEntityFrameworkStores<DataContext>()
+    .AddSignInManager<SignInManager<AppUser>>();
+
+var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+    builder.Configuration["TokenKey"]));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = key,
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+
+builder.Services.AddScoped<ITokenService, TokenService>();
+
 //! end extraction ~!
 
 var app = builder.Build();
