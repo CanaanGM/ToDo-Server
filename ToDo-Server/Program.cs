372eda894e651d41f5e27fe30c96bf0255b14c64
Canaan <reakak1@gmail.com>
2022-08-25 07:49:58 +0300

    Installed packages, configured automapper, sqlite db

--- ToDo-Server/Program.cs
+++ ToDo-Server/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+using ToDo_Server.Data.DbAccess;
+using ToDo_Server.Data.Mapper;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//! extract into an extension class
+builder.Services.AddAutoMapper(typeof(MapperProfiles).Assembly);
+builder.Services.AddDbContext<SqliteDbContext>(opt => opt.UseSqlite(
+    builder.Configuration["ConnectionStrings:SqliteTempBase"]
+    ));
+//! end extraction ~!
 
 var app = builder.Build();
 
