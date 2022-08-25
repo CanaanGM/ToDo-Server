413126f69dd2f92b2f89633f282f868a425969fe
Canaan <reakak1@gmail.com>
2022-08-25 07:58:05 +0300

    Add: Base class for all app controllers

---
+++ ToDo-Server/Controllers/BaseApiController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+
+using ToDo_Server.Application.Core;
+
+namespace ToDo_Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        //* handle custom return type 
+        protected ActionResult HandleResult<T>(Result<T> res)
+            => res == null
+            ? NotFound()
+            : res.IsSuccess && res.Value != null
+                ? Ok(res.Value)
+                : res.IsSuccess && res.Value == null
+                    ? NotFound()
+                    : BadRequest(res.Error);
+    }
+}
